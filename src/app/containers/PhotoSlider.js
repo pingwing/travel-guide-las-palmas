1f2f9b560d413d04e2e945aa18ace3124a1e17c1
Przemysław Gierski <pgierski@o2.pl>
2017-03-02 18:58:45 +0000

    use redux in PhotoSlider

--- src/app/containers/PhotoSlider.js
+++ src/app/containers/PhotoSlider.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Component} from 'react';
 import MiniPhoto from "./MiniPhoto";
+import {connect} from 'react-redux';
 
-export default class PhotoSlider extends Component {
+class PhotoSlider extends Component {
   render() {
+    console.log('PINGWIN: this.props', this.props);
     return (
       <ul className='pagination photoSlider' style={{height: '10%',display:'inline'}}>
         <li><a href="#">«</a></li>
@@ -35,3 +37,12 @@ export default class PhotoSlider extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    markers: state.markers,
+    currentMarker: state.currentMarker
+  };
+};
+
+export default connect(mapStateToProps)(PhotoSlider);
