c995d49734e84aecf339711fbc47a5cb11428fcb
Przemysław Gierski <pgierski@o2.pl>
2017-03-03 14:54:34 +0000

    add bottom clicking

--- src/app/containers/PhotoSlider.js
+++ src/app/containers/PhotoSlider.js
@@ -3,15 +3,15 @@ import {Component} from 'react';
 import MiniPhoto from "./MiniPhoto";
 import {connect} from 'react-redux';
 import Photo from './Photo';
-import selectMarker from '../actions';
+import { selectMarker, } from '../actions';
 
 class PhotoSlider extends Component {
-  onClick = (evt) => {
-    this.props.dispatch(selectMarker(evt.target.key))
+  onClick = (markerKey) => {
+    console.log('PINGWIN: markerKey', markerKey);
+    this.props.dispatch(selectMarker(markerKey))
   };
 
   render() {
-
     let currentMarkerObject;
     this.props.markers.forEach((marker) => {
       if (marker.key === this.props.currentMarker) {
@@ -28,7 +28,7 @@ class PhotoSlider extends Component {
           <ul className='pagination photoSlider' style={{height: '10%', display: 'inline'}}>
             <li><a style={{marginLeft:'30px'}} href="#">«</a></li>
             {this.props.markers.map((marker) => {
-              return <li><a href="#"><MiniPhoto click={this.onClick} imageUrl={marker.imageUrl}/></a></li>
+              return <li><a href="#"><MiniPhoto click={this.onClick} imageUrl={marker.imageUrl} markerKey={marker.key}/></a></li>
             })}
             <li><a style={{float: 'right', marginRight: '30px'}} href="#">»</a></li>
           </ul>
