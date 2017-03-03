cddd2ce6c51b960fad478081ca877d2b87ab301c
Ponton <tgierczynski@thebrain.pro>
2017-03-03 14:39:32 +0100

    onclick

--- src/app/containers/MiniPhoto.js
+++ src/app/containers/MiniPhoto.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 
 export default class MiniPhoto extends Component {
   render() {
-    return (<div>
+    return (<div onClick={this.props.click}>
       <img role="presentation" className="col col-lg image pull-left" style={{height:'20px',}} src={this.props.imageUrl}/>
       </div>
     )
