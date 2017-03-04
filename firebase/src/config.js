e4114f64772dcc689ec8dce0f225761dc28213be
Celano, Jose <jose.celano@hyve.net>
2017-03-04 10:23:22 +0000

    Remove firebase integration TODO sample

--- firebase/src/config.js
+++
@@ -1,9 +0,0 @@
-export const firebase = {
-  apiKey: 'AIzaSyCXS2Ka_o3p9OPg93pNNrMgvXUXEajLkNg',
-  authDomain: 'travel-guide-las-palmas.firebaseapp.com',
-  databaseURL: 'https://travel-guide-las-palmas.firebaseio.com',
-  storageBucket: 'travel-guide-las-palmas.appspot.com',
-  messagingSenderId: '643399313830'
-}
-
-export default { firebase }
